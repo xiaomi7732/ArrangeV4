3aef45cb909042cb698abcfbd8745ed715dbbe44
Saar Shen 🚾 <saars@microsoft.com>
2026-01-09 16:39:03 -0800

    Initial

---
+++ prototype/ConsoleProto/Native.cs
@@ -0,0 +1,36 @@
+using System.Runtime.InteropServices;
+
+public static class Native
+{
+    enum GetAncestorFlags
+    {
+        GetParent = 1,
+        GetRoot = 2,
+        /// <summary>
+        /// Retrieves the owned root window by walking the chain of parent and owner windows returned by GetParent.
+        /// </summary>
+        GetRootOwner = 3
+    }
+
+    /// <summary>
+    /// Retrieves the handle to the ancestor of the specified window.
+    /// </summary>
+    /// <param name="hwnd">A handle to the window whose ancestor is to be retrieved.
+    /// If this parameter is the desktop window, the function returns NULL. </param>
+    /// <param name="flags">The ancestor to be retrieved.</param>
+    /// <returns>The return value is the handle to the ancestor window.</returns>
+    [DllImport("user32.dll", ExactSpelling = true)]
+    static extern IntPtr GetAncestor(IntPtr hwnd, GetAncestorFlags flags);
+
+    [DllImport("kernel32.dll")]
+    static extern IntPtr GetConsoleWindow();
+
+    // This is your window handle!
+    public static IntPtr GetConsoleOrTerminalWindow()
+    {
+        IntPtr consoleHandle = GetConsoleWindow();
+        IntPtr handle = GetAncestor(consoleHandle, GetAncestorFlags.GetRootOwner);
+
+        return handle;
+    }
+}
---
+++ prototype/ConsoleProto/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Identity.Client;
+
+var scopes = new[] { "User.Read" };
+
+BrokerOptions options = new(BrokerOptions.OperatingSystems.Windows)
+{
+    Title = "My Awesome Application"
+};
+
+IPublicClientApplication app =
+    PublicClientApplicationBuilder.Create("0b4dbe1b-b67c-4ce1-b46b-d66832dc80b0")
+    .WithDefaultRedirectUri()
+    .WithParentActivityOrWindow(Native.GetConsoleOrTerminalWindow)
+    .WithBroker(options)
+    .Build();
+
+AuthenticationResult result = null;
+
+// Try to use the previously signed-in account from the cache
+IEnumerable<IAccount> accounts = await app.GetAccountsAsync();
+IAccount existingAccount = accounts.FirstOrDefault();
+
+try
+{    
+    if (existingAccount != null)
+    {
+        result = await app.AcquireTokenSilent(scopes, existingAccount).ExecuteAsync();
+    }
+    // Next, try to sign in silently with the account that the user is signed into Windows
+    else
+    {    
+        result = await app.AcquireTokenSilent(scopes, PublicClientApplication.OperatingSystemAccount)
+                            .ExecuteAsync();
+    }
+}
+// Can't get a token silently, go interactive
+catch (MsalUiRequiredException ex)
+{
+    result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+}
