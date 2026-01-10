4f10c90568bf37c5bc6cfe8a3ea73596159b1241
Saar Shen 🚾 <saars@microsoft.com>
2026-01-09 22:07:57 -0800

    Sign in works for the proto

--- prototype/ConsoleProto/Program.cs
+++ prototype/ConsoleProto/Program.cs
@@ -10,42 +10,42 @@ BrokerOptions options = new(BrokerOptions.OperatingSystems.Windows)
 
 IPublicClientApplication app =
     PublicClientApplicationBuilder.Create("0b4dbe1b-b67c-4ce1-b46b-d66832dc80b0")
-    .WithTenantId("2c952286-1d8a-461a-bde8-3b0447c7691d")
-    .WithRedirectUri("ms-appx-web://Microsoft.AAD.BrokerPlugin/0b4dbe1b-b67c-4ce1-b46b-d66832dc80b0")
+    .WithAuthority("https://login.microsoftonline.com/consumers", validateAuthority: true)
+    .WithDefaultRedirectUri()
     .WithParentActivityOrWindow(Native.GetConsoleOrTerminalWindow)
     .WithBroker(options)
     .Build();
 
-AuthenticationResult result = null;
+AuthenticationResult? result = null;
 
 // Try to use the previously signed-in account from the cache
 IEnumerable<IAccount> accounts = await app.GetAccountsAsync();
-foreach(var acct in accounts)
+foreach (var acct in accounts)
 {
     Console.WriteLine($"Found account: {acct.Username} ({acct.HomeAccountId?.Identifier}), env: {acct.Environment}");
 }
 
 IAccount? existingAccount = accounts.FirstOrDefault();
 
-// try
-// {    
-//     if (existingAccount != null)
-//     {
-//         result = await app.AcquireTokenSilent(scopes, existingAccount).ExecuteAsync();
-//     }
-//     // Next, try to sign in silently with the account that the user is signed into Windows
-//     else
-//     {    
-//         result = await app.AcquireTokenSilent(scopes, PublicClientApplication.OperatingSystemAccount)
-//                             .ExecuteAsync();
-//     }
+try
+{
+    if (existingAccount != null)
+    {
+        result = await app.AcquireTokenSilent(scopes, existingAccount).ExecuteAsync();
+    }
+    // Next, try to sign in silently with the account that the user is signed into Windows
+    else
+    {
+        result = await app.AcquireTokenSilent(scopes, PublicClientApplication.OperatingSystemAccount)
+                            .ExecuteAsync();
+    }
 
-// }
-// // Can't get a token silently, go interactive
-// catch (Exception ex)
-// {
-    result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
-// }
+}
+// Can't get a token silently, go interactive
+catch (Exception ex)
+{
+    result = await app.AcquireTokenInteractive(scopes).ExecuteAsync().ConfigureAwait(false);
+}
 
 Console.WriteLine($"Access token:\n{result.AccessToken}");
 Console.WriteLine($"Signed in user: {result.Account.Username} ({result.Account.HomeAccountId?.Identifier}), env: {result.Account.Environment}");
