a8b3acdf15786456bb6387c4d64a5cd6badb3f0e
Saar Shen <xiaomi7732@hotmail.com>
2026-01-10 21:57:26 -0800

    List all the calendars

--- prototype/ConsoleProto/Program.cs
+++ prototype/ConsoleProto/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.Graph;
 using Microsoft.Identity.Client;
 using Microsoft.Identity.Client.Broker;
 
-var scopes = new[] { "User.Read", "Calendars.ReadWrite" };
+string[] scopes = new[] { "User.Read", "Calendars.ReadWrite" };
 
 BrokerOptions options;
     options = new(BrokerOptions.OperatingSystems.Windows)
@@ -27,7 +27,7 @@ IPublicClientApplication app = appBuilder.Build();
 
 // Try to use the previously signed-in account from the cache
 IEnumerable<IAccount> accounts = await app.GetAccountsAsync();
-foreach (var acct in accounts)
+foreach (IAccount acct in accounts)
 {
     Console.WriteLine($"Found account: {acct.Username} ({acct.HomeAccountId?.Identifier}), env: {acct.Environment}");
 }
@@ -35,10 +35,26 @@ foreach (var acct in accounts)
 IAccount? existingAccount = accounts.FirstOrDefault();
 
 // Create GraphServiceClient with MSAL authentication
-var authProvider = new MsalAuthenticationProvider(app, scopes, existingAccount);
-var graphClient = new GraphServiceClient(authProvider);
+MsalAuthenticationProvider authProvider = new(app, scopes, existingAccount);
+GraphServiceClient graphClient = new(authProvider);
 
 // Example: Get user info
-var me = await graphClient.Me.GetAsync();
+Microsoft.Graph.Models.User? me = await graphClient.Me.GetAsync();
 Console.WriteLine($"\nDisplay Name: {me?.DisplayName}");
 Console.WriteLine($"Email: {me?.Mail ?? me?.UserPrincipalName}");
+
+// List all calendars:
+var calendars = await graphClient.Me.Calendars.GetAsync();
+Console.WriteLine("Calendars:");
+if (calendars?.Value != null)
+{
+    foreach (var calendar in calendars.Value)
+    {
+        Console.WriteLine($"- {calendar.Name} (ID: {calendar.Id})");
+    }
+}
+else
+{
+    Console.WriteLine("No calendars found.");
+}
+
